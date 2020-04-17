ff0ef00dae6541b969d428866a84cd24a663942b
Patrick Mifsud <patrickmifsud@outlook.com>
2020-04-17 23:55:46 +1000

    Initial Commit

---
+++ SwiftUI-UserDefults-Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-UserDefults-Demo
+//
+//  Created by Patrick Mifsud on 17/4/20.
+//  Copyright © 2020 Patrick Mifsud. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
